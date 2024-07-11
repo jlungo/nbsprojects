ff061fe3cd784637787c09435c9eaf5030e6ce6d
Juma Lungo <juma.lungo@zalongwa.com>
2024-07-11 13:27:42 +0300

    updated

---
+++ HelloBite.java
@@ -0,0 +1,12 @@
+class HelloBite{
+    public static void main (String[] args){
+        System.out.println(" ");
+        System.out.print("1."+" NBS");
+        System.out.print(", 2."+" NBS");
+        System.out.print(", 3."+" NBS");
+        System.out.print(", 4."+" NBS");
+        System.out.print(", 5."+" NBS");
+        System.out.println(" ");
+        System.out.println(" ");
+    }
+}
