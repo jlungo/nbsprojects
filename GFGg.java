a8e25e903d38d9d79a01584e741e6e7e723a44d2
Majangara <rose.michaelrd@gmail.com>
2024-07-16 15:57:27 +0300

    addnew pro

---
+++ GFGg.java
@@ -0,0 +1,22 @@
+class GFC{
+    static String Employee_name;
+    static Float Employee_salary;
+
+    static void Set(String n, Float P) { 
+        Employee_name = n;
+        Employee_salary = p;
+    }
+    static void get() {
+        System.out.println("Employee name is: " +Employee_name);
+        System.out.println("Employee CTC is: " +Employee_salary);
+    }
+    public static void main (String[] args){
+        GFC.Set ("Rathod Avinash", 1000.0f);
+        GFC.get();
+    }
+}
+
+    
+
+
+
